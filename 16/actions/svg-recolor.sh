cb98eaa0ea9e7b4789a2ece1a9a669369c861476
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-14 07:09:30 -0700

    a litany of changes

---
+++ 16/actions/svg-recolor.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for i in **/*.svg; do sed -i 's/#d24d57/#ff3d81/g' "$i" && echo "$i"; done
+
+
+
+for i in **/*.svg; do sed -i 's/#5d656b/#555e70/g' "$i"  && echo "$i"; done
+
+
+
+for i in **/*.svg; do sed -i 's/#919191/#555e70/g' "$i"  && echo "$i"; done
+
+
+for i in **/*.svg; do sed -i 's/#fdfdfd/#f4f4f7/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#b4b4b4/#b2bfd9/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#666/#556/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#fff/#f4f4f7/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#190000/#19171e/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#b5bbc0/#b2bfd9/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#b5bbc0/#b2bfd9/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#4c8fd8/#0badff/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#6ddce7/#00eaff/g' "$i"  && echo "$i"; done
+
+
+for i in **/*.svg; do sed -i 's/#71a5e8/#00ffcc/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#8dd786/#00ffcc/g' "$i"  && echo "$i"; done
+
+for i in **/*.svg; do sed -i 's/#71a575/#00ffcc/g' "$i"  && echo "$i"; done
