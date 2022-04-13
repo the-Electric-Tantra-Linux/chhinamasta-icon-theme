82efd4fbda88b1b989565e207afa3771d5dfd1fd
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2022-04-13 00:43:17 -0700

    optimized

--- opt.sh
+++ opt.sh
@@ -1,64 +1,27 @@
 #!/bin/bash
 
-cd 16x16/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+cd actions/16 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd actions/22 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd actions/24 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd actions/64 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+
 ##############################################################################
-cd 24x24/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+cd apps/scalable && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
 ##############################################################################
-cd 32x32/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+cd devices/16 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd devices/scalable && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
+
 ##############################################################################
-cd 48x48/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+
+cd emblems/24 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
 ##############################################################################
-cd 64x64/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+cd mimetypes/scalable && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
 ##############################################################################
-cd 128x128/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+
+cd places/16 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd places/scalable && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
 ##############################################################################
-cd 256x256/actions && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd apps && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd devices && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emblems && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd emotes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd mimetypes && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd places && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd ..
-cd status && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd..
+cd status/16 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd status/20 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd status/48 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
+cd status/64 && for i in *.svg; do svgo -i $i --pretty --multipass; done && cd .. && cd ..
